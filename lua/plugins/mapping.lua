ed6e4621337108ae6061ec22e625550fedb076ba
Alan Yang <alanyang54@gmail.com>
2025-03-05 12:13:25 +0800

    add nerd icon for language tools which key

--- lua/plugins/mapping.lua
+++ lua/plugins/mapping.lua
@@ -4,14 +4,14 @@ return {
     opts = {
       mappings = {
         n = {
-          ["<Leader>kj"] = { "<cmd>TSToolsAddMissingImports<cr>", desc = "Fixed all missing import" },
-          ["<Leader>ku"] = { "<cmd>TSToolsRemoveUnusedImports<cr>", desc = "Remove unused import" },
-          ["<Leader>kf"] = { "<cmd>TSToolsFixAll<cr>", desc = "Fixed all fixable errors" },
+          ["<Leader>kj"] = { "<cmd>TSToolsAddMissingImports<cr>", desc = "Typescript import missing" },
+          ["<Leader>ku"] = { "<cmd>TSToolsRemoveUnusedImports<cr>", desc = "Typescript remove unused import" },
+          ["<Leader>kf"] = { "<cmd>TSToolsFixAll<cr>", desc = "Typscript fix all" },
           ["<Leader>kk"] = { "<cmd>RustLsp codeAction<cr>", desc = "Rust code action" },
           ["<Leader>km"] = { "<cmd>RustLsp relatedDiagnostics<cr>", desc = "Rust diagnostics" },
           ["<Leader>kc"] = { "<cmd>RustLsp openCargo<cr>", desc = "Rust open cargo file" },
           ["<Leader>ki"] = { "<cmd>RustLsp hover actions<cr>", desc = "Rust hover actions" },
-          ["<Leader>k"] = { desc = "Language tools" },
+          ["<Leader>k"] = { desc = " Language tools" },
         },
       },
     },
