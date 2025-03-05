4965f09c13a949c5a5843a712e67a86a0d89adb9
Alan Yang <alanyang54@gmail.com>
2025-03-05 11:59:15 +0800

    named language tools which key

--- lua/plugins/mapping.lua
+++ lua/plugins/mapping.lua
@@ -4,11 +4,14 @@ return {
     opts = {
       mappings = {
         n = {
-          ["<Leader>kk"] = { "<cmd>TSToolsAddMissingImports<cr>", desc = "Fixed all missing import" },
+          ["<Leader>kj"] = { "<cmd>TSToolsAddMissingImports<cr>", desc = "Fixed all missing import" },
           ["<Leader>ku"] = { "<cmd>TSToolsRemoveUnusedImports<cr>", desc = "Remove unused import" },
           ["<Leader>kf"] = { "<cmd>TSToolsFixAll<cr>", desc = "Fixed all fixable errors" },
-          ["<Leader>kl"] = { "<cmd>RustLsp codeAction<cr>", desc = "Rust code action" },
-          desc = "Language tools",
+          ["<Leader>kk"] = { "<cmd>RustLsp codeAction<cr>", desc = "Rust code action" },
+          ["<Leader>km"] = { "<cmd>RustLsp relatedDiagnostics<cr>", desc = "Rust diagnostics" },
+          ["<Leader>kc"] = { "<cmd>RustLsp openCargo<cr>", desc = "Rust open cargo file" },
+          ["<Leader>ki"] = { "<cmd>RustLsp hover actions<cr>", desc = "Rust hover actions" },
+          ["<Leader>k"] = { desc = "Language tools" },
         },
       },
     },
