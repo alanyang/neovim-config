009834d85cdba9e4ac97b802421caad6452ca63c
Alan Yang <alanyang54@gmail.com>
2025-03-20 02:11:03 +0800

    fixed some bug

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -7,6 +7,52 @@ return {
   },
   init = function()
     require("codecompanion").setup {
+      display = {
+        chat = {
+          -- Change the default icons
+          icons = {
+            pinned_buffer = " ",
+            watched_buffer = "👀 ",
+          },
+
+          -- Alter the sizing of the debug window
+          debug_window = {
+            ---@return number|fun(): number
+            width = vim.o.columns - 5,
+            ---@return number|fun(): number
+            height = vim.o.lines - 2,
+          },
+
+          -- Options to customize the UI of the chat buffer
+          window = {
+            layout = "vertical", -- float|vertical|horizontal|buffer
+            position = "right", -- left|right|top|bottom (nil will default depending on vim.opt.plitright|vim.opt.splitbelow)
+            border = "single",
+            height = 0.8,
+            width = 0.5,
+            relative = "editor",
+            full_height = true, -- when set to false, vsplit will be used to open the chat buffer vs. botright/topleft vsplit
+            opts = {
+              breakindent = true,
+              cursorcolumn = false,
+              cursorline = true,
+              foldcolumn = "0",
+              linebreak = true,
+              list = false,
+              numberwidth = 1,
+              signcolumn = "no",
+              spell = false,
+              wrap = true,
+            },
+          },
+
+          ---Customize how tokens are displayed
+          ---@param tokens number
+          ---@param adapter CodeCompanion.Adapter
+          ---@return string
+          token_count = function(tokens, adapter) return " (" .. tokens .. " tokens)" end,
+        },
+      },
       adapters = {
         openai = function()
           return require("codecompanion.adapters").extend("openai", {
@@ -21,7 +67,7 @@ return {
           return require("codecompanion.adapters").extend("deepseek", {
             schema = {
               model = {
-                default = "deepseek-reasoner",
+                default = "deepseek-chat",
               },
             },
           })
